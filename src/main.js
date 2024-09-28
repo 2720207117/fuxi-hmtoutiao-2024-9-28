e3e129bc1538b40859d59dc26de6252fc5cf15c3
CJX2720207117 <2720207117@qq.com>
2024-09-28 22:12:02 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// main.js职责1：依赖项目需要的资源(js模块、css资源 ...)
+// main.js职责2：实例化跟实例。 render() 渲染App组件。 mount('#app')挂载到#app的容器内
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
