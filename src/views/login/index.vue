f30d6a58f2cab5e4c409e5cca9c33811687b1f0e
CJX2720207117 <2720207117@qq.com>
2024-09-30 21:41:02 +0800

    登录模块-02-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="login-container">
+    <el-card class="login_box">
+        <img src="@/assets/images/logo_index.png" alt="logo">
+    </el-card>
   </div>
 </template>
 
@@ -11,5 +13,25 @@ export default {
 </script>
 
 <style lang='less' scoped>
-
+    .login-container {
+        background: url('@/assets/images/login_bg.jpg') no-repeat top / cover;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        .login_box {
+            width: 420px;
+            height: 360px;
+            position: relative;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            img {
+                display: block;
+                width: 200px;
+                margin: 0 auto;
+            }
+        }
+    }
 </style>
